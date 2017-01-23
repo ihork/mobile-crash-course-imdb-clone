c96c655b1cc3ac4f1fbc4605f160a60a9c2e50d8
ihork <ihork@wix.com>
2017-01-23 14:18:12 +0200

    Style up FilmDetails screen

--- src/containers/FilmDetails/index.js
+++ src/containers/FilmDetails/index.js
@@ -11,15 +11,25 @@ class FilmDetails extends Component {
 
     render() {
         const {film} = this.props;
-        const imageUrl = filmsDbService.getImageUrl(film.poster_path);
+        const imageUrl = filmsDbService.getImageUrl(film.poster_path);console.log('Film', film);
         return (
             <View style={stylesheet.filmItem}>
-                <Image
-                    style={stylesheet.image}
-                    source={{uri: imageUrl}}
-                />
                 <Text style={stylesheet.title}>{film.title}</Text>
-                <Text style={stylesheet.year}>{` (${film.release_date.split('-')[0]}) `}</Text>
+                <View style={stylesheet.info}>
+                    <Image
+                        style={stylesheet.image}
+                        source={{uri: imageUrl}}
+                    />
+                    <View>
+                        <Text style={stylesheet.details}>{`Release date: ${film.release_date}`}</Text>
+                        <Text style={stylesheet.details}>{`Average Vote: ${film.vote_average}`}</Text>
+                        <Text style={stylesheet.details}>{`Count of Votes: ${film.vote_count}`}</Text>
+                        <Text style={stylesheet.details}>{`Popularity: ${film.popularity}`}</Text>
+                        <Text style={stylesheet.details}>{`Is Adult: ${film.adult}`}</Text>
+                        <Text style={stylesheet.details}>{`Language: ${film.original_language}`}</Text>
+                    </View>
+                </View>
+                <Text style={stylesheet.overview}>{film.overview}</Text>
             </View>
         );
     }
--- src/containers/FilmDetails/stylesheet.js
+++ src/containers/FilmDetails/stylesheet.js
@@ -2,23 +2,30 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     filmItem: {
-        flexDirection: 'row',
         flex: 1,
         backgroundColor: '#ccc',
-        padding: 15,
+        padding: 15
     },
     title: {
-        fontSize: 15,
-        maxWidth: 200,
+        fontSize: 17,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        maxWidth: 300,
         color: '#333'
     },
-    year: {
-        fontSize: 11,
+    info: {
+        flexDirection: 'row'
+    },
+    details: {
+        fontSize: 13,
         color: '#666'
     },
     image: {
         width: 120,
         height: 180,
-        marginRight: 5
+        marginRight: 10
+    },
+    overview: {
+        marginTop: 10
     }
 });
